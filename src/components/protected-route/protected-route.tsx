87515ced1110947eaef83d78f67b9ee94f82bfc2
mylittletraf <kanviser@gmail.com>
2025-06-13 21:54:51 +0300

    ProtectedRoute updated

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,5 +1,27 @@
+import { ReactElement } from 'react';
+import { useSelector } from '../../services/store';
+import { getUserState } from '../../slices/userSlice/userSlice';
+import { Navigate, useLocation } from 'react-router-dom';
+
 type ProtectedRouteProps = {
-  children: React.ReactElement;
+  children: ReactElement;
+  unAuthRequired?: boolean;
 };
 
-export const ProtectedRoute = ({ children }: ProtectedRouteProps) => children;
+export const ProtectedRoute = ({
+  children,
+  unAuthRequired = false
+}: ProtectedRouteProps) => {
+  const location = useLocation();
+  const { isAuthenticated } = useSelector(getUserState);
+
+  if (isAuthenticated && unAuthRequired) {
+    return <Navigate to='/' replace />;
+  }
+
+  if (!isAuthenticated && !unAuthRequired) {
+    return <Navigate to='/login' state={{ from: location }} replace />;
+  }
+
+  return children;
+};
