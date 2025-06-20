32cb8f7dcc4211639a8b6a111e2499950eec1d35
Andrey Boyko (macbook) <mylittletraf@gmail.com>
2025-06-20 18:00:14 +0300

    add jest config moduleNameMapper

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,18 @@ import type { Config } from 'jest';
 
 const config: Config = {
   preset: 'ts-jest',
-  coverageProvider: 'v8'
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@pages(.*)$': '<rootDir>/src/pages$1',
+    '^@components(.*)$': '<rootDir>/src/components$1',
+    '^@ui(.*)$': '<rootDir>/src/components/ui$1',
+    '^@ui-pages(.*)$': '<rootDir>/src/components/ui/pages$1',
+    '^@utils-types(.*)$': '<rootDir>/src/utils/types$1',
+    '^@api$': '<rootDir>/src/utils/burger-api.ts',
+    '^@slices(.*)$': '<rootDir>/src/services/slices$1',
+    '^@selectors(.*)$': '<rootDir>/src/services/selectors$1'
+  }
 };
 
 export default config;
